4ce54cf1e66c1de2a512fde37c0ae980482ab2fb
alejandroturres <alejandroturres@gmail.com>
2017-09-11 00:32:28 -0300

    p2 init

---
+++ config/auth.js
@@ -0,0 +1,12 @@
+module.exports = {
+	'facebookAuth': {
+		'clientID': 'xxxxx',
+		'clientSecret': 'yyyyy',
+		'callbackURL': 'http://localhost:1333/auth/facebook/callback'
+	},
+	'googleAuth': {
+		'clientID': '450858325324-ptuvc8a02ub8b0k1bk3p3h9ob21771fe.apps.googleusercontent.com',
+		'clientSecret': 'rmGXE09X1pAEZetzRG30TukZ',
+		'callbackURL': 'http://localhost:1333/auth/google/callback'
+	}
+}
---
+++ config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+	'url' : 'mongodb://alejandro:alejandro@ds161410.mlab.com:61410/alejandro'
+}
